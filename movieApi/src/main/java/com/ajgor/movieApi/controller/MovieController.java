85776cf9236c2052cae9c5103112aed87fec8969
ajgors <igor.stadnicki@gmail.com>
2023-07-06 21:31:22 +0200

    sorting movies and pagination of movies

--- movieApi/src/main/java/com/ajgor/movieApi/controller/MovieController.java
+++ movieApi/src/main/java/com/ajgor/movieApi/controller/MovieController.java
@@ -3,25 +3,26 @@ package com.ajgor.movieApi.controller;
 import com.ajgor.movieApi.dto.MovieRequest;
 import com.ajgor.movieApi.dto.MovieResponse;
 import com.ajgor.movieApi.exception.MovieNotFoundException;
-import com.ajgor.movieApi.entity.Movie;
 import com.ajgor.movieApi.service.MovieService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
 
     private final MovieService movieService;
+    private final String DEFAULT_PAGE_NUMBER = "0";
+    private final String DEFAULT_PAGE_SIZE = "10";
+    private final String DEFAULT_SORTED_BY = "id";
+    private final String DEFAULT_SORTED_DIR = "asc";
 
     @Autowired
-    public MovieController(MovieService movieService){
+    public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
 
@@ -31,12 +32,17 @@ public class MovieController {
     }
 
     @GetMapping
-    public ResponseEntity<List<MovieResponse>> getMovies() {
-        return ResponseEntity.ok(movieService.getMovies());
+    public ResponseEntity<Page<MovieResponse>> getMovies(
+            @RequestParam(required = false, defaultValue = DEFAULT_PAGE_NUMBER) Integer page,
+            @RequestParam(required = false, defaultValue = DEFAULT_PAGE_SIZE) Integer size,
+            @RequestParam(required = false, defaultValue = DEFAULT_SORTED_BY) String sortedBy,
+            @RequestParam(required = false, defaultValue = DEFAULT_SORTED_DIR) String sortedDir
+    ) {
+        return ResponseEntity.ok(movieService.getMovies(page, size, sortedBy, sortedDir));
     }
 
     @PostMapping
-    public ResponseEntity<MovieRequest> putMovie(@RequestBody @Valid MovieRequest movie){
+    public ResponseEntity<MovieRequest> putMovie(@RequestBody @Valid MovieRequest movie) {
         return new ResponseEntity<>(movieService.putMovie(movie), HttpStatus.CREATED);
     }
 }
--- movieApi/src/main/java/com/ajgor/movieApi/service/MovieService.java
+++ movieApi/src/main/java/com/ajgor/movieApi/service/MovieService.java
@@ -2,14 +2,13 @@ package com.ajgor.movieApi.service;
 
 import com.ajgor.movieApi.dto.MovieRequest;
 import com.ajgor.movieApi.dto.MovieResponse;
-import com.ajgor.movieApi.exception.MovieNotFoundException;
 import com.ajgor.movieApi.entity.Movie;
+import com.ajgor.movieApi.exception.MovieNotFoundException;
 import com.ajgor.movieApi.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
@@ -17,22 +16,28 @@ public class MovieService {
     private final MovieRepository movieRepository;
 
     @Autowired
-    public MovieService(MovieRepository movieRepository){
+    public MovieService(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
     }
 
-    public List<MovieResponse> getMovies(){
-        return movieRepository.findAll().stream()
-                .map(MovieResponse::new)
-                .collect(Collectors.toList());
+    public Page<MovieResponse> getMovies(Integer page, Integer size, String sortedBy, String sortedDir) {
+
+        Sort sort = sortedDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortedBy).ascending() : Sort.by(sortedBy).descending();
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        List<MovieResponse> movies = movieRepository.findAll(pageable).stream()
+                .map(MovieResponse::new).toList();
+
+        return new PageImpl<>(movies, pageable, movieRepository.count());
     }
 
-    public MovieResponse getMovie(Long id){
+    public MovieResponse getMovie(Long id) {
         Movie movie = movieRepository.findById(id).orElseThrow(() -> new MovieNotFoundException(id));
         return new MovieResponse(movie);
     }
 
-    public MovieRequest putMovie(MovieRequest movie){
+    public MovieRequest putMovie(MovieRequest movie) {
         Movie movieToSave = Movie.builder()
                 .title(movie.getTitle())
                 .description(movie.getDescription())
